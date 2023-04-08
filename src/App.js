b44094c9881a852511dbe5733222cb83a18f0f75
murat <murat.tc2014@gmail.com>
2023-04-08 16:39:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import data from './data'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [holidays, setHolidays] = useState(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+    <h3>National Holidays Dates</h3>
+    {holidays.map((holiday)=>{
+      return(
+        <div className='holiday' key={holiday.id}>
+          <img src={holiday.img} alt="" />
+          <div>
+            <p>{holiday.title}</p>
+            <p>{holiday.date}</p>
+          </div>
+        </div>
+      )
+    })}
+    <button onClick={()=>setHolidays([])}>Clear All</button>
+      </section>
+      
+    </main>
   );
 }
 
